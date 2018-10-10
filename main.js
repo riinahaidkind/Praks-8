50585b2ce8a067a143c66f4f8bff7a46a9197839
Riina Häidkind <riina.haidkind@khk.ee>
2018-10-10 16:26:38 +0300

    Javascript kahe arvu korraga sisestamiseks

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 let numInput = document.querySelector('#numInput');
 let respOutput = document.querySelector('#respOutput');
+let incInput = document.querySelector('#incInput');
 
 const getFact = () => {
+    respOutput.innerHTML = ''
     let number = numInput.value;
+    let inc = incInput.value;
+    let baseUrl = 'http://numbersapi.com/'
+    // vaja saada sarnane URL
+    // http://numbersapi.com/1..3
+    
+    let url = `${baseUrl}${number}..${parseInt(number) + parseInt(inc)}`;
 
-    if (number !='') {
-    axios.get('http://numbersapi.com/'+number)
+    if (number !='' && inc != '') {
+    axios.get(url)
     .then(response => {
-        respOutput.innerText = response.data;
+        for (let item in response.data){
+            let li = document.createElement('li');
+            li.innerText = response.data[item];
+            
+            respOutput.appendChild(li);
+        }
     })
+    
     .catch(error => {
         console.log(error);
     })
  }
+    
 }
     
 // Listening to events
-    numInput.addEventListener('input', getFact);
+numInput.addEventListener('input', getFact);
+incInput.addEventListener('input', getFact);
